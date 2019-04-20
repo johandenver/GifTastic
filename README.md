edb1f36cd7374b797fdf35c1cf24fe8cbbccc624
JohanDenver <48187072+johandenver@users.noreply.github.com>
2019-04-19 20:56:49 -0700

    Updated files, js, html, css, images, readme.

--- README.md
+++ README.md
@@ -1,106 +1,41 @@
-# GifTastic
-App to pull from Giphy API. 
+# Giftastic!
 
+App to pull from Giphy API using JavaScript for the logic and jQuery to manipulate HTML. App includes AJAX method and CSS.
 
-# GifTastic
+<img src="assets/images/app.jpg">
 
-### Overview
+To view live application:   https://johandenver.github.io/GifTastic/
 
-In this assignment, you'll use the GIPHY API to make a dynamic web page that populates with gifs of your choice. To finish this task, you must call the GIPHY API and use JavaScript and jQuery to change the HTML of your site.
+1. App allows user to pull a number of gifs on any topic from the Giphy API.
 
-![GIPHY](Images/1-giphy.jpg)
+2. The User submits a topic to create new topic button, and also chooses the number of gifs to display.
 
-### Before You Begin
+3. If the user clicks on a topic button, the page will display the desired number of non-animated gif images from the GIPHY API. 
 
-1. **Hit the GIPHY API**.
-   * Fool around with the GIPHY API. [Giphy API](https://developers.giphy.com/docs/).
-   * Be sure to read about these GIPHY parameters (hint, hint):
-     * `q`
-     * `limit`
-     * `rating`
-   * Like many APIs, GIPHY requires developers to use a key to access their API data. To use the GIPHY API, you'll need a GIPHY account (don't worry, it's free!) and then obtain an API Key by [creating an app](https://developers.giphy.com/dashboard/?create=true).
-   * Make sure you switch the protocol in the query URL from **`http to https`**, or the app may not work properly when deployed to Github Pages.
+4. If the user clicks on the still gif it will animate. A second click will stop animation. 
 
-2. **[Watch the demo video](https://youtu.be/BqreERTLjgQ)**
+5. Under every gif, the rating is displayed. This app has a rating limit of "PG". 
 
-   * You should have a high-level understanding of how this assignment works before attempting to code it.
+## Getting Started
 
-### Submission on BCS
+Download zip file.
 
-* Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
+Open file in IDE of choice to view code.
 
-### Instructions
+Open index.html in browser to view application.
 
-1. Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
-   * We chose animals for our theme, but you can make a list to your own liking.
+## Built With
 
-2. Your app should take the topics in this array and create buttons in your HTML.
-   * Try using a loop that appends a button for each string in the array.
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - The programming language used for all logic of the    game.
+* [Bootstrap](https://getbootstrap.com/) - Used components from toolkit for developing the HTML layout. 
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/css) - used for styling of the application.
+* [Google Fonts](https://fonts.google.com/) - Used to generate font styles.
+* [Giphy API](https://developers.giphy.com/) - API used to generate gif files.
 
-3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+## Authors
 
-4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+* Jonathan Peine - Initial work - [Github](https://github.com/johandenver)
 
-5. Under every gif, display its rating (PG, G, so on).
-   * This data is provided by the GIPHY API.
-   * Only once you get images displaying with button presses should you move on to the next step.
+## Acknowledgments
 
-6. Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-
-7. Deploy your assignment to Github Pages.
-
-8. **Rejoice**! You just made something really cool.
-
-- - -
-
-### Minimum Requirements
-
-Attempt to complete homework assignment as described in instructions. If unable to complete certain portions, please pseudocode these portions to describe what remains to be completed. Adding a README.md as well as adding this homework to your portfolio are required as well and more information can be found below.
-
-- - -
-
-### Bonus Goals
-
-1. Ensure your app is fully mobile responsive.
-
-2. Allow users to request additional gifs to be added to the page.
-   * Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
-
-3. List additional metadata (title, tags, etc) for each gif in a clean and readable format.
-
-4. Include a 1-click download button for each gif, this should work across device types.
-
-5. Integrate this search with additional APIs such as OMDB, or Bands in Town. Be creative and build something you are proud to showcase in your portfolio
-
-6. Allow users to add their favorite gifs to a `favorites` section.
-   * This should persist even when they select or add a new topic.
-   * If you are looking for a major challenge, look into making this section persist even when the page is reloaded(via localStorage or cookies).
-
-### Reminder: Submission on BCS
-
-* Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
-
-- - -
-
-### Create a README.md
-
-Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
-
-* [About READMEs](https://help.github.com/articles/about-readmes/)
-
-* [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
-
-- - -
-
-### Add To Your Portfolio
-
-After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
-
-- - -
-
-### One More Thing
-
-If you have any questions about this project or the material we have covered, please post them in the community channels in slack so that your fellow developers can help you! If you're still having trouble, you can come to office hours for assistance from your instructor and TAs.
-
-**Good Luck!**
-
+* Would love to give a shout out to the TAs, Instructors, Tutor and study partners! 
--- assets/app.js
+++ assets/app.js
@@ -1,147 +1,178 @@
 // create function to load page before .js runs:
-$(document).ready(function(){
 
-// Create Variables for initial four categories:
-var categories = ["flying", "school", "coding", "pizza"];
+$(document).ready(function() {
+    
+// Create Variables for initial four gif categories which will load to the page:
 
+var initialTopics = ["rocket", "star wars", "space", "star trek", "galaxy", "bowie", "martian"];
+
+// Loop to add initial topics array to html as buttons using createButton function:
+
+for (i = 0; i < initialTopics.length; i++) {
+    createButton(initialTopics[i]);
+}
+
+// click event on gif button to have the gif images appear:
+
+$(document).on("click", ".gif-button", function(){
+    // captures the data name of the gif button being clicked:
+    var gifs = $(this).attr("data-name");
+    // captures the number of gifs the user has requested:
+    var number = $("#search-amount").val().trim();
+    // ensures the rating of the gifs are at least PG rated: 
+    var rating = "PG";
+    // call the gitGitData() to run the ajax from API:
+    getGifData(gifs, number, rating);
+
+}) 
+
+// click event to change gif images from still to animated:
+
+$(document).on("click", ".image-gif", function(){
+    // set the attribute of the item to variable name:
+    var state = $(this).attr("data-status");
+    // if image-gif is in a still state, will switch to animated state:
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-status", "animate");
+    }
+    // if image-gif is in animated state, it will move back to still state:
+    else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-status", "still");
+    }
+});
+
+
+// Click event for "submit" button to capture input and create new gif button:
+
+$("#submit-button").on("click", function(event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var gif = $("#gif-topics").val().trim().toLowerCase();
+    //validation:
+    if(gif !== "" & !initialTopics.includes(gif)){
+        initialTopics.push(gif);
+       // create next gif button:
+        createButton(gif);   
+    } else {
+        $("#alert-message").slideDown("slow")
+    }
+    $("#gif-topics").val("");
+  
+});
+
+$("#gif-topics").click(function(){
+    $("#alert-message").slideUp("slow")
+})
+
+
+// This is the gitGifData function to pull from Giphy API using my Ajax key:0ni8MlatpXTG9gDcbcA823so5bLAKWUb:
+
+function getGifData(gifs, number, rating) {
+    //notes: 
+    //var gif = $("gif-topics").val().trim();
+    //var number = $("#search-amount").val().trim();
+    //var rating = "PG"
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + gifs + "&api_key=0ni8MlatpXTG9gDcbcA823so5bLAKWUb&limit=" + number + "&rating=" + rating +"&lang=en";
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).then(function(response) {
+        //create variable name for the data response received from API:
+        var gifResponse = response.data;
+        // Log the resulting object
+        console.log(gifResponse);
+        $("#gif-view").empty();
+
+        // loop through response array to find the neede items:
+        for (let i=0; i < gifResponse.length; i++) {
+            //new div for gifs:
+            var gifsDiv = $("<div class='gif text-center'>");
+            var imageGif;
+            //create a rating element:
+            ratingGif = $("<p>");
+            //attach text to rating element:
+            ratingGif.text("Gif rating is " + gifResponse[i].rating.toUpperCase());
+            //create an image element for the gif:
+            imageGif = $("<img>");
+            //attache attributes and class:
+            imageGif.attr("src", gifResponse[i].images.fixed_height_still.url);
+            imageGif.attr("data-still", gifResponse[i].images.fixed_height_still.url);
+            imageGif.attr("data-animate", gifResponse[i].images.fixed_height.url);
+            //imageGif.attr("data-status", "still");
+            imageGif.addClass("image-gif")
+            imageGif.attr("alt", gifResponse[i].title);
+            // append the elements to the gifs div created above:
+            gifsDiv.append(imageGif, ratingGif);
+            // prepend the above gifs div to the div id on the HTML page:
+            $("#gif-view").prepend(gifsDiv);
+
+        }
+        
+        
+      });
+
+};
+
+// create the createButton function to create the dynamic buttons:
+
+function createButton(gif){
+    // dynamic created button in a variable:
+    var gifButton = $("<button>");
+    // add a class to button variable:
+    gifButton.addClass("topic-btn rounded gif-button");
+    // add an attribute to button variable:
+    gifButton.attr("data-name", gif);
+    // add text to button variable: 
+    gifButton.text(gif);
+    // Adding the button to the buttons-view div
+    $("#gif-button").append(gifButton, " ");
+    
+};
+
+});
+
+
+// notes: 
 // $("#search-input") is the id for the input field for the search topics
 // $("#search-amount") is the id for the input field for the amount of gifs requested. 
 // $("#button-view") is the id for where the dynamic created buttons append. 
 // $("#gif-view") is the id for where the gifs from the API append. 
 
-// function to pull from Giphy API using my Ajax key:0ni8MlatpXTG9gDcbcA823so5bLAKWUb:
-/*
-function apiPull() {
+/* my first attempted method: 
 
-    // Constructing a URL to search Giphy for the name of the person who said the quote
-    var topic = "";
-
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=0ni8MlatpXTG9gDcbcA823so5bLAKWUb&limit=5";
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-      })
-        
-      .then(function(response) {
-
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-
-      });
-
-};
-
-apiPull();
-
-//apiPull();
-*/
-// function to manipulate the response from the API pull. needs to call function apiPull()
-function processData(){
-  // need to dynamically create a div:
-  var gifArea = $("<div class='gify'>");
-
-  var rating = $("<div class='rating'>");
-
-  var stillGif = "data-state"
-  stillGif.attr("data-state", "still")
-
-  var animatedGif = "data-state"
-  animatedGif.attr("data-state", "animated")
-
-  img 
-
-  gifArea.append(rating, stillGif, animatedGif)
-
-$("#gif-view").append(gifArea);
-
-//apiPull();
-
-};
-
-// function to create the dynamic buttons:
-function createButton(){
+function createButton(gif){
 
     $("#button-view").empty();
 
-    for (i = 0; i < categories.length; i++) {
+    for (i = 0; i < initialTopics.length; i++) {
 
-        var movieButton = $("<button>");
+        var gifButton = $("<button>");
         // add a class to button
-        movieButton.addClass("topic-btn rounded");
+        gifButton.addClass("topic-btn rounded");
         // add an attribute to button
-        movieButton.attr("data-name", categories[i]);
-        // add text to button
-        movieButton.text(categories[i]);
-        // Adding the button to the buttons-view div
-        $("#button-view").append(movieButton);
+        gifButton.attr("data-name", initialTopics[i]);
         
-    }
+        // add text to button
+        gifButton.text(initialTopics[i]);
+        // Adding the button to the buttons-view div
+        $("#button-view").append(gifButton, " ");
+        
+    };
 
 };
 
-// create click event for "submit" button to capture input and then call function to add dynamic topic button with input.
-$("submit-button").click(function(event) {
+$("#submit-button").on("click", function() {
     event.preventDefault();
 
     // This line grabs the input from the textbox
-    var input = $("#search-input").val().trim();
+    var topic = $("#search-input").val().trim();
 
     // Add topic from the text input to the array
-    categories.push(input);
+    initialTopics.push(topic);
 
     createButton();
 });
-
-/*
-      // This function handles events where a movie button is clicked
-      $("#add-movie").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var movie = $("#movie-input").val().trim();
-
-        // Adding movie from the textbox to our array
-        movies.push(movie);
-
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
 */
-
-// create click event to pull the gif from API upon the topic button click:
-//$("topic-btn").click()
-
-
-// create click event on gifs to move from still to animated.
-/*
-$(".gif").on("click", function() {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
-  });
-*/
-
-createButton();
-
-
-});
-
-
-
-
-
-
-
-
